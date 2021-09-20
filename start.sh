0304ee5e6a6ce41ad39656010c4310100974474d
freestyletear <freestyletear@gmail.com>
2021-09-20 14:12:09 +0300

    initial heroku commit

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-export SECRET_KEY='kj29'
-export MAIL_USERNAME='freestyletear@gmail.com'
-export MAIL_PASSWORD='kj9261$$'
+export MAIL_USERNAME='wreckthatonepitchlikeralph@gmail.com'
+export MAIL_PASSWORD='kj1234$$'
 export DATABASE_URL='postgresql+psycopg2://username:password@localhost/pitchit'
 
 python3 manage.py server
